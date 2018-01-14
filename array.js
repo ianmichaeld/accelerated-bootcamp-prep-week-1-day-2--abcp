07af718e4fbf6c033d223de8bf1bb533bb54460e
Ian Dewsbury <ianmichaeld@gmail.com>
2018-01-14 17:22:27 +0000

    automatically backed up by learn

--- array.js
+++ array.js
@@ -7,31 +7,45 @@ function addElementToBeginningOfArray(array, element) {
 
 function destructivelyAddElementToBeginningOfArray(array, element){
   // add element to the beginning of the array by mutating the array
+  array.unshift(element);
+  return array;
 }
 
 function addElementToEndOfArray(array, element) {
-
+  var a = [...array];
+  a.push(element);
+  return a;
 }
 
 function destructivelyAddElementToEndOfArray(array, element) {
-
+  array.push(element);
+  return array;
 }
 
 function accessElementInArray(array, index) {
-
+  return array[index];
 }
 
 function destructivelyRemoveElementFromBeginningOfArray(array) {
+  array.shift();
+  return array;
 
 }
 
 function removeElementFromBeginningOfArray(array) {
+  var a = [...array];
+  a.shift();
+  return a;
 }
 
 function destructivelyRemoveElementFromEndOfArray(array) {
-
+  array.pop();
+  return array;
 }
 
 function removeElementFromEndOfArray(array) {
+  var a = [...array];
+  a.pop();
+  return a;
 
 }
